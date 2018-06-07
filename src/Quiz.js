0e527cc1e7d4fc4dd945927962f5ef030f2d0777
Jason Nakamura <jaynak@gmail.com>
2018-06-06 21:51:23 -0400

    Mod 7

--- src/Quiz.js
+++ src/Quiz.js
@@ -14,6 +14,10 @@ class Quiz extends Component {
         //console.log(quizData);
     }
 
+    handleResetClick() {
+        this.setState({quiz_position: 1})
+    }
+
     showNextQuestion() {
         this.setState((state) => {
             return { quiz_position: state.quiz_position+1 }
@@ -24,7 +28,7 @@ class Quiz extends Component {
     render() {
         const isQuizEnd = this.state.quiz_position - 1 === quizData.quiz_questions.length;
         return ( <div>
-            { isQuizEnd ? <QuizEnd /> :
+            { isQuizEnd ? <QuizEnd resetClickHandler = {this.handleResetClick.bind(this)}/> :
             <QuizQuestion quiz_question = { quizData.quiz_questions[
                 this.state.quiz_position - 1] } 
                 showNextQuestionHandler = {this.showNextQuestion.bind(this)}/>
